b494796b21ec9f8fbb850207f4fc91a6be1831c4
GUO FENG <guofeng20094@gmail.com>
2020-07-20 00:04:00 +0800

    Update short_statements.py

--- short_statements.py
+++ short_statements.py
@@ -1,3 +1,18 @@
 def remove_fc(state_dict):
     """ Remove the fc layer parameter from state_dict. """
   return {key: value for key, value in state_dict.items() if not key.startswith('fc.')}
+
+#generate list of names
+peta_dataset.image_name = [f'{100000 + i + 1:06}.png' for i in range(19000)]
+
+#rename files
+
+for count, filename in enumerate(os.listdir(peta_dataset.root)): 
+    new_name = f'{100000 + i + 1:06}.png'
+
+    src = os.path.join(peta_dataset.root, filename) 
+    dst = os.path.join(peta_dataset.root, new_name) 
+
+    os.rename(src, dst) 
+    i = i + 1
+   
